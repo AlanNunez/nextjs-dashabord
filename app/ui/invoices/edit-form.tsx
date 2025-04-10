3c2bad492581544ec3232f926b74751a8f93cf4b
AlanNunez <raul.nunez01@gmail.com>
2025-04-10 10:45:04 -0400

    Initial commit

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,14 +1,14 @@
 'use client';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import { Button } from '@/app/ui/button';
+import Link from 'next/link';
 import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
-import Link from 'next/link';
-import { Button } from '@/app/ui/button';
 
 export default function EditInvoiceForm({
   invoice,
